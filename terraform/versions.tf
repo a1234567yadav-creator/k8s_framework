21b643395fecfaa633e254333aad487ffae53546
Ajay Yadav <TP68063@lv.com>
2025-12-03 01:24:11 +0530

    state

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -20,6 +20,6 @@ terraform {
     resource_group_name  = "1000055123-rg"
     storage_account_name = "k8splatform"
     container_name       = "tfstate"
-    key                  = "state/dev-aks.tfstate"
+    key                  = "dev-aks.tfstate"
   }
 }
